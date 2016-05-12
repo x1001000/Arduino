bde84717e5bd827bbf59ed664395982300387eab
x1001000 <x1001000@users.noreply.github.com>
2016-05-12 20:30:01 +0800

    write T

--- cylindrical-4axis-parallel/cylindrical-4axis-parallel.ino
+++ cylindrical-4axis-parallel/cylindrical-4axis-parallel.ino
@@ -2,46 +2,26 @@
 #include <Servo.h>
 Servo servoNumber[4];
 
-float L = 8.1, R = 8.1;
-float thetaL, thetaR;
-float P, H, D;
-
-int servoConfig[4][5] = {
+const float P0 = 90, H0 = 1, D0 = 12;
+const float L = 8.1, R = 8.1;
+const int servoConfig[4][5] = {
   //outputPin, initAngle, minAngle, maxAngle, inputPin
   { 10, 180, 90, 180, 0},                               //左
-  { 11, map(90,  0, 180,  2, 159),   0, 180, 1},        //下
+  {  3, map(90,  0, 180,  2, 156),   0, 180, 1},        //下
   {  9, map(90, 45, 180, 45, 150),  45, 180, 2},        //右
   {  8, 157, 144, 170, 3},                              //前
 };
 
-void setup() {
-  Serial.begin(9600);
-  for (int i = 0; i < 4; i++) {
-    servoNumber[i].attach(servoConfig[i][0]);
-    servoNumber[i].write(servoConfig[i][1]);
-  }
-}
-
-void loop() {
-  while (Serial.available() == 0) {}
-  P =  Serial.parseFloat();
-  Serial.println(P);
-  while (Serial.available() == 0) {}
-  H = Serial.parseFloat();
-  Serial.println(H);
-  while (Serial.available() == 0) {}
-  D = Serial.parseFloat();
-  Serial.println(D);
-
-  H -= 9.5;
+void servo(float P, float H, float D) {
+  float thetaL, thetaR;
   float f1, f2 = 0;
   for (int theta = 900; theta <= 1800; theta++) {
     thetaL = (float)theta / 10 * PI / 180;
     thetaR = thetaL + 2 * atan(0 - H / D);
-    f1 = 8 * cos(thetaR) + 8 * cos(thetaL) + D;
+    f1 = L * cos(thetaL) + R * cos(thetaR) + D;
     if (f1 * f2 < 0) {
-      Serial.println(f2, 6);
-      Serial.println(f1, 6);
+      //Serial.println(f2, 6);
+      //Serial.println(f1, 6);
       break;
     }
     f2 = f1;
@@ -51,6 +31,22 @@ void loop() {
   thetaR = map(thetaR, 45, 180, 45, 150);
   servoNumber[0].write(thetaL);
   servoNumber[2].write(thetaR);
-  P = map(P, 0, 180,  2, 159);
-  servoNumber[1].write(P);
+  servoNumber[1].write(map(P,  0, 180,  2, 156));
+  delay(50);
+}
+
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < 4; i++) {
+    servoNumber[i].attach(servoConfig[i][0]);
+    servoNumber[i].write(servoConfig[i][1]);
+  }
+  delay(1000);
+}
+
+void loop() {
+  for (int P = 120; P >= 60; P -= 4) servo(P, H0, D0 / cos((P - 90) * PI / 180));
+  setup();
+  for (float D = D0; D >= 4; D -= 0.4) servo(P0, H0, D);
+  setup();
 }
