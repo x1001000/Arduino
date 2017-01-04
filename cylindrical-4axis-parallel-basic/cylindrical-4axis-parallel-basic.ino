7d7894fa6a2c4e77fcd0f05a05553284532e9fea
十百千 <x1001000@users.noreply.github.com>
2017-01-04 13:31:21 +0800

    Mission Accomplished

--- cylindrical-4axis-parallel-basic/cylindrical-4axis-parallel-basic.ino
+++ cylindrical-4axis-parallel-basic/cylindrical-4axis-parallel-basic.ino
@@ -1,4 +1,3 @@
-// 範圍限制尚未處理
 #include <Servo.h>
 Servo servo[4];
 int inputSignal[4];
@@ -6,9 +5,9 @@ int outputAngle[4];
 const int servoConfig[4][5] = {
   //outputPin, initAngle, minAngle, maxAngle, inputPin
   { 11, 135,  90, 180, 0}, //左
-  { 10, 125,  70, 180, 2}, //右
+  { 10, 115,  50, 180, 2}, //右
   {  9, 150, 135, 165, 3}, //前
-  {  8, 85,    5, 155, 1}, //下
+  {  4,  85,   0, 180, 1}, //下  http://playground.arduino.cc/Main/ArduinoPinCurrentLimitations
 };
 void measureRangeOfServo(int i) {
   int value = 0;
@@ -17,20 +16,28 @@ void measureRangeOfServo(int i) {
     value *= 10;
     value += Serial.read() - '0';
   }
-  if (value != 0) servo[i].write(value);
+  if (value != 0)
+    servo[i].write(value);
   delay(100);
 }
 void joystickInput() {
   for (int i = 0; i < 4; i++)
-    inputSignal[i] = map(analogRead(servoConfig[i][4]), 0, 1023, -1, 2);
+    inputSignal[i] = map(analogRead(servoConfig[i][4]), 0, 1023, -4, 5);
 }
 void servoOutput() {
   for (int i = 0; i < 4; i++) {
-    if (inputSignal[i]) {
-      outputAngle[i] -= inputSignal[i];   //配合下servo的轉向
-      servo[i].write(outputAngle[i]);
-      delay(10);
-    }
+    if (i == 1)
+      inputSignal[i] *= -1;                                         //修正右servo轉向
+    if (inputSignal[i])
+      if (outputAngle[i] - inputSignal[i] > servoConfig[i][2])
+        if (outputAngle[i] - inputSignal[i] < servoConfig[i][3]) {  //預檢有無超出範圍
+          outputAngle[i] -= inputSignal[i];                         //配合左下servo轉向
+          servo[i].write(outputAngle[i]);                           //4or5段速
+          if (i == 3)
+            delay(700);                                             //下servo快轉會當機
+          else
+            delay(20);
+        }
   }
 }
 void setup() {
