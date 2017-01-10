e94047cf4295a8df13b1ffbb0502035e4c6955a1
十百千 <x1001000@users.noreply.github.com>
2017-01-10 20:26:58 +0800

    timer

--- keypad-74595-7segment/keypad-74595-7segment.ino
+++ keypad-74595-7segment/keypad-74595-7segment.ino
@@ -10,25 +10,24 @@
 // Downloaded from: https://circuits.io/circuits/3660379-arduino-keypad
 
 #include <Keypad.h>
-const byte ROWS = 4;
-const byte COLS = 4;
-byte rowPins[ROWS] = { 9, 8, 7, 6 };
-byte colPins[COLS] = { 5, 4, 3, 2 };
 
-char keys[ROWS][COLS] = {
+byte rowPins[] = { 9, 8, 7, 6 };
+byte colPins[] = { 5, 4, 3, 2 };
+
+char keys[][4] = {
   {'1', '2', '3', 'A'},
   {'4', '5', '6', 'B'},
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
 
-Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, 4, 4 );
 
-const byte clockPin = 11;  //Pin connected to SH_CP of 74HC595
-const byte latchPin = 12;  //Pin connected to ST_CP of 74HC595
-const byte dataPin = 13;   //Pin connected to DS    of 74HC595
+byte clockPin = 11; //Pin connected to SH_CP of 74HC595
+byte latchPin = 12; //Pin connected to ST_CP of 74HC595
+byte dataPin = 13;  //Pin connected to DS    of 74HC595
 
-const byte code[] = {	// for Common Cathode 7-segment
+const byte code[] = { // for Common Cathode 7-segment
   B11111100,
   B01100000,
   B11011010,
@@ -42,18 +41,54 @@ const byte code[] = {	// for Common Cathode 7-segment
 };
 
 char key, sevenSegment[3];
+int doomTime;
 
-void display() {
+void getAllzeros() {
+  sevenSegment[0] = '0';
+  sevenSegment[1] = '0';
+  sevenSegment[2] = '0';
+  updateSevenSegment();
+}
+
+void getBack() {
+  sevenSegment[2] = sevenSegment[1];
+  sevenSegment[1] = sevenSegment[0];
+  sevenSegment[0] = 0;      // code[ 0 - '0' ] == 0
+  updateSevenSegment();
+}
+
+void getClear() {
+  sevenSegment[0] = 0;
+  sevenSegment[1] = 0;
+  sevenSegment[2] = 0;
+  updateSevenSegment();
+}
+
+void getDoom() {
+  doomTime = 1;
+  if (sevenSegment[0] - '0' >= 0) doomTime += (sevenSegment[0] - '0') * 100;
+  if (sevenSegment[1] - '0' >= 0) doomTime += (sevenSegment[1] - '0') * 10;
+  if (sevenSegment[2] - '0' >= 0) doomTime += (sevenSegment[2] - '0') * 1;
+  while (doomTime--) {
+    sevenSegment[0] = '0' + doomTime / 100;
+    sevenSegment[1] = '0' + (doomTime % 100) / 10;
+    sevenSegment[2] = '0' + doomTime % 10;
+    updateSevenSegment();
+    delay(1000);
+  }
+}
+
+void updateSevenSegment() {
   digitalWrite(latchPin, LOW);
-  shiftOut( dataPin, clockPin, LSBFIRST, 255 ); // Common Anode
-  shiftOut( dataPin, clockPin, LSBFIRST, 255 );	// Common Anode
-  shiftOut( dataPin, clockPin, LSBFIRST, 255 ); // Common Anode
+  shiftOut( dataPin, clockPin, LSBFIRST, 255 ); // because of CA
+  shiftOut( dataPin, clockPin, LSBFIRST, 255 ); // because of CA
+  shiftOut( dataPin, clockPin, LSBFIRST, 255 ); // because of CA
   digitalWrite(latchPin, HIGH);
-  delay(100);								// clear
+  delay(100);     // avoid Persistence of vision
   digitalWrite(latchPin, LOW);
-  shiftOut( dataPin, clockPin, LSBFIRST, 255 - code[sevenSegment[2] - '0'] ); // to Common Anode
-  shiftOut( dataPin, clockPin, LSBFIRST, 255 - code[sevenSegment[1] - '0'] ); // to Common Anode
-  shiftOut( dataPin, clockPin, LSBFIRST, 255 - code[sevenSegment[0] - '0'] ); // to Common Anode
+  shiftOut( dataPin, clockPin, LSBFIRST, 255 - code[sevenSegment[0] - '0'] ); // to Common Anode 7-segment
+  shiftOut( dataPin, clockPin, LSBFIRST, 255 - code[sevenSegment[1] - '0'] ); // to Common Anode 7-segment
+  shiftOut( dataPin, clockPin, LSBFIRST, 255 - code[sevenSegment[2] - '0'] ); // to Common Anode 7-segment
   digitalWrite(latchPin, HIGH);
 }
 
@@ -61,22 +96,26 @@ void setup() {
   pinMode( clockPin, OUTPUT );
   pinMode( latchPin, OUTPUT );
   pinMode( dataPin, OUTPUT );
-  digitalWrite(latchPin, LOW);
-  shiftOut( dataPin, clockPin, LSBFIRST, 255 ); // Common Anode
-  shiftOut( dataPin, clockPin, LSBFIRST, 255 );	// Common Anode
-  shiftOut( dataPin, clockPin, LSBFIRST, 255 ); // Common Anode
-  digitalWrite(latchPin, HIGH);
+  getClear();
   Serial.begin(9600);
 }
 
 void loop() {
   if (key = kpd.getKey()) {
     Serial.println(key);
-    if ( key - '0' >= 0 && key - '0' <= 9 ) {
-      sevenSegment[2] = sevenSegment[1];
-      sevenSegment[1] = sevenSegment[0];
-      sevenSegment[0] = key;
-      display();
+    if (key == 'A')
+      getAllzeros();
+    else if (key == 'B')
+      getBack();
+    else if (key == 'C')
+      getClear();
+    else if (key == 'D')
+      getDoom();
+    else {
+      sevenSegment[0] = sevenSegment[1];
+      sevenSegment[1] = sevenSegment[2];
+      sevenSegment[2] = key;
+      updateSevenSegment();
     }
   }
 }
